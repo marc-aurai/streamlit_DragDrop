3e12d09f06913bf4f548d303a48785eaa4c1b49a
marc-aurai <marc@aurai.com>
2023-05-03 10:35:23 +0200

    Add new package name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="Streamlit-DragDrop",
+    name="Streamlit-DragDropList",
     version="0.0.1",
     author="Marc B",
     author_email="none@gmail.com",
