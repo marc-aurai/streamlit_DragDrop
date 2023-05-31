53773c4647af64358777ce42f077c8f34a9f785c
marc-aurai <marc@aurai.com>
2023-05-31 16:43:42 +0200

    Update version in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="Streamlit-DragDropList",
-    version="0.0.1",
+    version="0.0.2",
     author="Marc B",
     author_email="none@gmail.com",
     description="Streamlit component of draggable list",
